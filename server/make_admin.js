743cf51d821afecdded5217717edb5ac81b56010
Agya-Kwesi Manu-Ofosu <kwesiadom875@gmail.com>
2026-03-01 07:47:47 +0000

    chore: add verify_user script and update FRONTEND_URL hint

--- server/make_admin.js
+++ server/make_admin.js
@@ -8,7 +8,7 @@ const dbAddress = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/parfum_del
 mongoose.connect(dbAddress)
   .then(async () => {
     console.log('Connected to DB');
-    const email = 'adomagyakwesi1@gmail.com';
+    const email = 'agyakwesiadom@gmail.com';
     const res = await User.updateOne({ email: email }, { $set: { isAdmin: true } });
     console.log(`Updated ${email} to admin:`, res);
     process.exit();
